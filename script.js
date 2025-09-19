ebfea2fda1ed20c4ef923600fa14f4ec7bee77e7
aveencoder <aveen598@gmail.com>
2025-09-19 13:45:50 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
- document.addEventListener("DOMContentLoaded", function() {
-      // Create a text node with the message
-      const msg = document.createTextNode("DOM load success");
-      // Append the text node directly to the body
-      document.body.appendChild(msg);
-    });
